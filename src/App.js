02416693af561e0351f57bc9bc027f81b9d24090
John Stephens-Webb <JohnStephensWebb@Johns-MacBook-Pro-2.local>
2018-07-23 10:06:40 -0600

    Finally

--- src/App.js
+++ src/App.js
@@ -1,21 +1,145 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import Toolbar from './Components/Toolbar'
+import ComposeForm from './Components/ComposeForm'
+import MessageList from './Components/MessageList'
+
+import data from './MessageData'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+  constructor () {
+    super()
+    this.state = {
+      messages: data || []
+    }}
 
-export default App;
+
+  toggleSelected = (message) => {
+    const messages = [...this.state.messages]
+    let messageID = message.props.content.id
+    for (let i = 0; i < messages.length; i++) {
+      if (messages[i]['id'] === messageID) {
+        if (messages[i]['selected'] === false || messages[i]['selected'] === undefined) {
+          messages[i]['selected'] = true
+        }
+        else {
+          messages[i]['selected'] = false
+        }
+      }
+    }
+    this.setState({...this.state, messages: messages})}
+
+  toggleStarred = (message) => {
+      let messageID = message.props.content.id
+      const messages = [...this.state.messages]
+      for (let i = 0; i < messages.length; i++) {
+        if (messages[i]['id'] === messageID) {
+          if (messages[i]['starred'] === false || messages[i]['starred'] === undefined) {
+            messages[i]['starred'] = true
+          }
+          else {
+            messages[i]['starred'] = false
+          }
+        }
+      }
+      this.setState({messages: messages})}
+
+  selectAll = () => {
+    const messages = [...this.state.messages]
+    for (let i = 0; i < messages.length; i++) {
+      messages[i]['selected'] = true
+    }
+    this.setState({messages: messages})}
+
+  selectNone = () => {
+    const messages = [...this.state.messages]
+    for (let i = 0; i < messages.length; i++) {
+      messages[i]['selected'] = false
+    }
+    this.setState({messages: messages})
+  }
+
+  markRead = () => {
+    const messages = [...this.state.messages]
+    for (let i = 0; i < messages.length; i++) {
+      if (messages[i]['selected'] === true) {
+        messages[i]['read'] = true
+      }
+    }
+    this.setState({messages: messages})
+  }
+
+  markUnread = () => {
+    const messages = [...this.state.messages]
+    for (let i = 0; i < messages.length; i++) {
+      if (messages[i]['selected'] === true) {
+        messages[i]['read'] = false
+      }
+    }
+    this.setState({messages: messages})
+  }
+
+  remove = () => {
+    const messages = [...this.state.messages]
+    const newMessages = messages.filter( (message) => message['selected'] !== true)
+    this.setState({messages: newMessages})
+  }
+
+  addLabel = (label) => {
+    if (label !== 'Apply label') {
+      const messages = [...this.state.messages]
+      for (let i = 0; i < messages.length; i++) {
+        if (messages[i]['selected'] === true) {
+          if (messages[i]['labels'].includes(label) !== true) {
+            messages[i]['labels'].push(label)
+          }
+        }
+      }
+      this.setState({messages: messages})
+    }
+  }
+
+  removeLabel = (label) => {
+    const messages = [...this.state.messages]
+    for (let i = 0; i < messages.length; i++) {
+      if (messages[i]['selected'] === true) {
+        const newLabels = messages[i]['labels'].filter( (i) => i !== label)
+        messages[i]['labels'] = newLabels
+      }
+    }
+    this.setState({messages: messages})
+  }
+
+  render() {return (
+
+
+      <div className="main">
+
+
+        <Toolbar
+          data={this.state.messages}
+          selectAll={this.selectAll}
+          selectNone={this.selectNone}
+          markRead={this.markRead}
+          markUnread={this.markUnread}
+          remove={this.remove}
+          addLabel={this.addLabel}
+          removeLabel={this.removeLabel}
+          />
+
+
+
+
+        <MessageList
+        details={this.state.messages}
+        toggleSelected={this.toggleSelected}
+        toggleStarred={this.toggleStarred}
+        />
+
+        <ComposeForm />
+
+      </div>
+
+    )}
+}
+export default App
---
+++ src/MessageData.js
@@ -0,0 +1,53 @@
+const data = [
+    {
+      "id": 1,
+      "subject": "Test1",
+      "read": false,
+      "starred": true,
+      "labels": ["Personal"]
+    },
+    {
+      "id": 2,
+      "subject": "Test2",
+      "read": false,
+      "starred": false,
+      "labels": []
+    },
+    {
+      "id": 3,
+      "subject": "Test3",
+      "read": false,
+      "starred": true,
+      "labels": ["Dev"]
+    },
+    {
+      "id": 4,
+      "subject": "Test4",
+      "read": true,
+      "starred": false,
+      "labels": []
+    },
+    {
+      "id": 5,
+      "subject": "HAK THE PLANE-IT!",
+      "read": false,
+      "starred": false,
+      "labels": ["Galvanize"]
+    },
+    {
+      "id": 6,
+      "subject": "Test5",
+      "read": true,
+      "starred": true,
+      "labels": []
+    },
+    {
+      "id": 7,
+      "subject": "Test6",
+      "read": true,
+      "starred": false,
+      "labels": ["Personal"]
+    },
+  ]
+
+  export default data
